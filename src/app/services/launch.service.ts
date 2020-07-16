f1334b1b904fe31fb49664332f39f34e66f62ff1
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2020-07-16 20:23:53 +0100

    Base to create Http Requests in LaunchService

--- src/app/services/launch.service.ts
+++ src/app/services/launch.service.ts
@@ -1,9 +1,23 @@
+import { Launch } from './../models/Launch';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class LaunchService {
+  url: string = 'https://api.spacexdata.com/v4';
+
+  private launchesSource = new BehaviorSubject<Launch[]>([]);
+  launchesReference = this.launchesSource.asObservable();
 
   constructor() { }
 }
