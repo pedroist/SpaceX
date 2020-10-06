b484fe50bea57fa4b6a1bc9c4aa9a6b844b19c8e
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2020-10-06 18:41:44 +0100

    Removed all the default tests. We ll start them from scratch with Jest

--- src/app/pipes/show-only-success.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowOnlySuccessPipe } from './show-only-success.pipe';
-
-describe('ShowOnlySuccessPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShowOnlySuccessPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
